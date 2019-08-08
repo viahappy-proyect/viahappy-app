6fbd40c0c62257041fd7e6577be51286f49f23b7
Carlos Kruger <carloskr@yahoo.com>
2019-08-08 12:55:24 +0200

    pop

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash = require("connect-flash");
 
 
 mongoose
-  .connect(process.env.DB_LOCAL, { useNewUrlParser: true })
+  .connect(process.env.DB_REMOTE, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
--- bin/seeds.js
+++ bin/seeds.js
@@ -11,7 +11,7 @@ const User = require("../models/User");
 const bcryptSalt = 10;
 
 mongoose
-  .connect(process.env.DB_LOCAL, {useNewUrlParser: true})
+  .connect(process.env.DB_REMOTE, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
